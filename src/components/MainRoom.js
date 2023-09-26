d43dc9f314289e66eea76810cef234c7a64ed8f8
tannerdavison <tboydavison@gmail.com>
2023-09-25 21:40:34 -0600

    unmounted setMessages() on useEffect in mainJs

--- src/components/MainRoom.js
+++ src/components/MainRoom.js
@@ -1,77 +1,90 @@
 import { useState, useEffect } from "react";
 
 const MainRoom = ({ setMainAccess, mainAccess, userInfo, socket }) => {
-  const [message, setMessage] = useState("");
-  const [messageRecieved, setMessageRecieved] = useState([]);
-  const [room, setRoom] = useState(1);
+	const [message, setMessage] = useState("");
+	const [messageRecieved, setMessageRecieved] = useState([]);
+	const [room, setRoom] = useState(1);
 
-  const joinRoom = () => {
-    socket.emit("join_room", { room: room, username: userInfo.username, message: message});
-  };
+	const joinRoom = () => {
+		socket.emit("join_room", {
+			room: room,
+			username: userInfo.username,
+			message: message,
+		});
+	};
 
-  const sendUserInfo = (userInfo) => {
-    socket.emit("user_info", userInfo);
-  };
-  const leaveMain = () => {
-    setMainAccess(false);
-    socket.emit("leave", room);
-  };
-  const sendMessageFunc = (event) => {
-    event.preventDefault();
-    socket.emit("send_message", {
-      message: message,
-      room,
-      timestamp: new Date().toISOString(),
-    });
-  };
+	const sendUserInfo = (userInfo) => {
+		socket.emit("user_info", userInfo);
+	};
+	const leaveMain = () => {
+		setMainAccess(false);
+		socket.emit("leave", room);
+	};
+	const sendMessageFunc = () => {
+		socket.emit("send_message", {
+			message: message,
+			room,
+			timestamp: new Date().toISOString(),
+		});
+	};
 
-  useEffect(() => {
-   if (!socket) return; // Prevent code from running if socket is null or undefined
-	if(mainAccess ===true){
-	joinRoom();
-	setMainAccess("undefined");
-	sendUserInfo(userInfo);
+	useEffect(() => {
+		if (!socket) return; // Prevent code from running if socket is null or undefined
+
+		if (mainAccess === true) {
+			joinRoom();
+			setMainAccess("undefined");
+			sendUserInfo(userInfo);
 		}
-    socket.on("receive_message", (data) => {
-      setMessageRecieved((prev) => [...prev, data.message, data.timestamp]);
-      console.log(messageRecieved);
-    });
-  }, [socket]);
 
-  return (
-    <div className="App">
-      <div className="header">
-        <div className={"room-num-input"}>
-          <input
-            placeholder="Room #"
-            value={room}
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button type="submit" onClick={joinRoom}>
-            Join Room
-          </button>
-        </div>
-        <div className={"room-num-input"}>
-          <input
-            placeholder="Message..."
-            onChange={(event) => {
-              setMessage(event.target.value);
-            }}
-          />
-          <button onClick={sendMessageFunc}>Send Message</button>
-        </div>
-        <h1>Converstion: </h1>
-        <h1>
-          {messageRecieved.map((messages, index) => {
-            console.log(messages.messages);
-            return <p key={index}>{messages}</p>;
-          })}
-        </h1>
-      </div>
-      <button onClick={leaveMain}>Leave Room</button>
-    </div>
-  );
+		const handleReceiveMessage = (data) => {
+			setMessageRecieved((prev) => [...prev, data.message]);
+			console.log(messageRecieved);
+		};
+
+		socket.on("receive_message", handleReceiveMessage);
+
+		// Cleanup listener when component is unmounted or dependencies change
+		return () => {
+			socket.off("receive_message", handleReceiveMessage);
+		};
+	}, [socket]);
+
+	return (
+		<div className='App'>
+			<div className='header'>
+				<div className={"room-num-input"}>
+					<input
+						placeholder='Room #'
+						value={room}
+						onChange={(event) => {
+							setRoom(event.target.value);
+						}}
+					/>
+					<button
+						type='submit'
+						onClick={joinRoom}>
+						Join Room
+					</button>
+				</div>
+				<div className={"room-num-input"}>
+					<input
+						placeholder='Message...'
+						onChange={(event) => {
+							setMessage(event.target.value);
+						}}
+					/>
+					<button onClick={sendMessageFunc}>Send Message</button>
+				</div>
+				<h1>Converstion: </h1>
+				<div>
+					{messageRecieved.map((messages, index) => {
+						return <p key={index}>{messages}</p>;
+					})}
+				</div>
+			</div>
+			<button onClick={leaveMain}>Leave Room</button>
+		</div>
+	);
 };
 export default MainRoom;
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,40 +1,54 @@
 import { useState } from "react";
-import './Login.css';
+import "./Login.css";
 
+const Login = ({ sendUserInfo, loginToggle }) => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 
-const Login = ({sendUserInfo, loginToggle}) =>{
-const [username, setUsername] =useState('');
-const [password, setPassword] = useState('');
+	const submitHandler = (event) => {
+		event.preventDefault();
+		sendUserInfo(username, password);
 
-const submitHandler =(event) =>{
-    event.preventDefault();
-    sendUserInfo(username,password);
+		loginToggle("false");
+	};
+	return (
+		<>
+			<form onSubmit={submitHandler}>
+				<h1>New to Chatty-Chatter? </h1>
+				<h2>Create a Free Account!</h2>
 
-    loginToggle('false')
-}
-    return (
-      <>
-        <form onSubmit={submitHandler}>
-          <h1>New to Chatty-Chatter? </h1>
-          <h2>Create a Free Account!</h2>
+				<div className={"CreateUser"}>
+					<div className={"userLoginElements"}>
+						<label htmlFor='username-id'> Create Username :</label>
+						<input
+							type='text'
+							name={"usernameInput"}
+							onChange={(event) => setUsername(event.target.value)}
+							id='username-id'
+						/>
+					</div>
 
-          <div className={'CreateUser'}>
-            <div className={"userLoginElements"}>
-              <label htmlFor="username-id"> Create Username :</label>
-              <input type="text" name={"usernameInput"} onChange={event=>setUsername(event.target.value)}id="username-id" />
-            </div>
+					<div className={"userLoginElements"}>
+						<label htmlFor='password-id'> Create Password :</label>
+						<input
+							type='text'
+							name={"passwordInput"}
+							onChange={(event) => {
+								setPassword(event.target.value);
+							}}
+							id='password-id'
+						/>
+					</div>
 
-            <div className={"userLoginElements"}>
-              <label htmlFor="password-id"> Create Password :</label>
-              <input type="text" name={"passwordInput"} onChange={(event)=>{setPassword(event.target.value)}}id="password-id" />
-            </div>
-
-            <button className={'loginFormSubmitBtn'}type="submit"onClick={submitHandler}>Create New User</button>
-          </div>
-          
-        </form>
-      </>
-    );
-
-}
+					<button
+						className={"loginFormSubmitBtn"}
+						type='submit'
+						onClick={submitHandler}>
+						Create New User
+					</button>
+				</div>
+			</form>
+		</>
+	);
+};
 export default Login;
