e11cb0bc1de9a2219dd9ccdfd72e0d320c9ec07f
Tanner Davison <Tboydavison@gmail.com>
2023-09-26 01:50:58 -0600

    fixed bugs and styling

--- src/components/MainRoom.js
+++ src/components/MainRoom.js
@@ -1,90 +1,161 @@
 import { useState, useEffect } from "react";
+import './MainRoom.css'
+
+
+
 
 const MainRoom = ({ setMainAccess, mainAccess, userInfo, socket }) => {
-	const [message, setMessage] = useState("");
-	const [messageRecieved, setMessageRecieved] = useState([]);
-	const [room, setRoom] = useState(1);
 
-	const joinRoom = () => {
-		socket.emit("join_room", {
-			room: room,
-			username: userInfo.username,
-			message: message,
-		});
-	};
 
-	const sendUserInfo = (userInfo) => {
-		socket.emit("user_info", userInfo);
-	};
-	const leaveMain = () => {
-		setMainAccess(false);
-		socket.emit("leave", room);
-	};
-	const sendMessageFunc = () => {
-		socket.emit("send_message", {
-			message: message,
-			room,
-			timestamp: new Date().toISOString(),
-		});
-	};
+  const [message, setMessage] = useState("");
+  const [messageRecieved, setMessageRecieved] = useState([]);
+  const [room, setRoom] = useState(1);
 
-	useEffect(() => {
-		if (!socket) return; // Prevent code from running if socket is null or undefined
+  const joinRoom = () => {
+    socket.emit("join_room", {
+      room: room,
+      username: userInfo.username,
+      message: message,
+    });
+  loadRoomHistory();
+  };
+const loadRoomHistory = async () => {
+  const response = await fetch(`http://localhost:3001/roomHistory/${room}`);
+  const data = await response.json();
 
-		if (mainAccess === true) {
-			joinRoom();
-			setMainAccess("undefined");
-			sendUserInfo(userInfo);
-		}
+  // assuming the server returns an array of messages
+  if (data.messageHistory) {
+    setMessageRecieved(data.messageHistory);
+    console.log(messageRecieved)
+  }
+};
+  const sendUserInfo = (userInfo) => {
+    socket.emit("user_info", userInfo);
+  };
+  const leaveMain = () => {
+    setMainAccess(false);
+    socket.emit("leave", room);
+  };
+  const sendMessageFunc = () => {
+    // Emit the message
+    const newMessage = {
+      message: message,
+      room,
+      timestamp: getCurrentTime(),
+      username: userInfo.username, // add this line
+    };
 
-		const handleReceiveMessage = (data) => {
-			setMessageRecieved((prev) => [...prev, data.message]);
-			console.log(messageRecieved);
-		};
+    socket.emit("send_message", newMessage);
 
-		socket.on("receive_message", handleReceiveMessage);
+    // Update local state to include the new message
+    setMessageRecieved((prev) => [...prev, newMessage]);
+    setMessage("");
+   
+  };
+  const getCurrentTime = () => {
+    const date = new Date();
+    let year = String(date.getFullYear())
+    const month = String(date.getMonth() +1)
+    let hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    
+    const strTime = `${month}/${year} @${hours}:${minutes} ${ampm}`;
+    return strTime;
+  };
+ 
+  useEffect(() => {
+    if (!socket) return; // Prevent code from running if socket is null or undefined
 
-		// Cleanup listener when component is unmounted or dependencies change
-		return () => {
-			socket.off("receive_message", handleReceiveMessage);
-		};
-	}, [socket]);
+    if (mainAccess === true) {
+      joinRoom();
+      setMainAccess("undefined");
+      sendUserInfo(userInfo);
+    }
+    
+    const handleReceiveMessage = (data) => {
+      setMessageRecieved((prev) => [
+        ...prev,
+        {
+          message: data.message,
+          username: data.username,
+          timestamp: getCurrentTime(),
+        },
+      ]);
+    };
+    setMessage("");
+    socket.on("receive_message", handleReceiveMessage);
 
-	return (
-		<div className='App'>
-			<div className='header'>
-				<div className={"room-num-input"}>
-					<input
-						placeholder='Room #'
-						value={room}
-						onChange={(event) => {
-							setRoom(event.target.value);
-						}}
-					/>
-					<button
-						type='submit'
-						onClick={joinRoom}>
-						Join Room
-					</button>
-				</div>
-				<div className={"room-num-input"}>
-					<input
-						placeholder='Message...'
-						onChange={(event) => {
-							setMessage(event.target.value);
-						}}
-					/>
-					<button onClick={sendMessageFunc}>Send Message</button>
-				</div>
-				<h1>Converstion: </h1>
-				<div>
-					{messageRecieved.map((messages, index) => {
-						return <p key={index}>{messages}</p>;
-					})}
-				</div>
-			</div>
-			<button onClick={leaveMain}>Leave Room</button>
-		</div>
-	);
+    return () => {
+      socket.off("receive_message", handleReceiveMessage);
+    };
+    // eslint-disable-next-line
+  }, [socket, messageRecieved]);
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div className={"room-num-input"}>
+          <input
+            placeholder="Room #"
+            value={room}
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button type="submit" onClick={joinRoom}>
+            Join Room
+          </button>
+        </div>
+        <div className={"room-num-input"}>
+          <input
+            placeholder={message !== "" ? message: "Message..."}
+            onChange={(event) => {
+              setMessage(event.target.value);
+            }}
+          />
+          <button onClick={sendMessageFunc}>Send Message</button>
+        </div>
+        <h1>Converstion: </h1>
+        <div>
+          {messageRecieved.map((msg, index) => {
+            console.log(msg.message)
+            if (msg.username === userInfo.username) {
+              // Message sent by current user
+              return (
+                <>
+                  <div className={"messageBlockWrapper"}>
+                    <div key={index} className={"messagesContainer sender"}>
+                      <h3>{msg.message}</h3>
+                    </div>
+                    <p className={"username sender"}>{msg.username} </p>
+                    <p>{msg.timestamp}</p>
+                  </div>
+                </>
+              );
+            } else {
+              // Message received from another user
+              return (
+                <>
+                  <div className={"messageBlockWrapper"}>
+                    <div key={index} className={"messagesContainer receiver"}>
+                      <h3 className={"message_body"}>{msg.message}</h3>
+                    </div>
+                    <div className={"username receiver"}>
+                      <p>{msg.username}</p>
+                    </div>
+                    <p className={"timestamp"}>{msg.timestamp}</p>
+                  </div>
+                </>
+              );
+            }
+          })}
+        </div>
+      </div>
+      <button onClick={leaveMain}>Leave Room</button>
+    </div>
+  );
 };
 export default MainRoom;
