6bf71f94f6f02a81116f1fa86c1719e1409c9e26
tannerdavison <tboydavison@gmail.com>
2023-09-25 21:40:02 -0600

    unmounted setMessage function on useEffect

--- server/index.js
+++ server/index.js
@@ -4,150 +4,153 @@ const http = require("http");
 const mongoose = require("mongoose");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const mainRooms = require('./move')
+const Room = mainRooms.Rooms
+
 const url =
-  "mongodb+srv://tboydavison:Luliann465$@gendervsgender.iid016a.mongodb.net/";
+	"mongodb+srv://tboydavison:Luliann465$@gendervsgender.iid016a.mongodb.net/";
 
 app.use(cors());
 
 const server = http.createServer(app);
 
 async function connect() {
-  try {
-    await mongoose.connect(url);
-    console.log("Connected To MongoDB");
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		await mongoose.connect(url);
+		console.log("Connected To MongoDB");
+	} catch (err) {
+		console.log(err);
+	}
 }
 connect();
 
 const messagesSchema = new mongoose.Schema({
-  serverUserID: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  messages: String,
-  review: String,
+	serverUserID: {
+		type: String,
+		required: true,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	messages: String,
+	review: String,
 });
 
 const roomSchema = new mongoose.Schema({
-  room_number: {
-    type: Number,
-    required: true,
-  },
-  sent_by_user: {
-    type: mongoose.Schema.Types.ObjectId, // Using ObjectId for referencing
-    ref: "Message", // reference to the Message model
-    required: true,
-  },
-  username:{
-    type: String,
-    required:true,
-  },
-  message:{
-    type:String
-  }
+	room_number: {
+		type: Number,
+		required: true,
+	},
+	sent_by_user: {
+		type: mongoose.Schema.Types.ObjectId, // Using ObjectId for referencing
+		ref: "Message", // reference to the Message model
+		required: true,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	message: {
+		type: String,
+	},
 });
 
-const User = mongoose.model("User", messagesSchema);
 const Rooms = mongoose.model("Rooms", roomSchema);
+const User = mongoose.model("User", messagesSchema);
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "DELETE", ""],
-  },
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST", "DELETE", ""],
+	},
 });
 
 let numUsers = 0;
 io.on("connection", (socket) => {
-  console.log(`User Connected:${socket.id}`);
-  numUsers++;
-  console.log({ NUMBEROFUSERS: numUsers });
+	console.log(`User Connected:${socket.id}`);
+	numUsers++;
+	console.log({ NUMBEROFUSERS: numUsers });
 
-socket.on("user_info", async (data) => {
- User.findOne({ serverUserID: socket.id }).then((user)=>{
-if (!user) {
-    const user = new User({
-      serverUserID: `${socket.id}`,
-      username: `${data.username}`,
-      messages: "",
-    });
-    user.save();
- }})
-  
-});
-  socket.on("join_room", (data) => {
-    socket.join(data.room);
-    
-  User.findOne({ username: data.username }).then((user) => {
-    if (!user) {
-      console.log(`No user found with username: ${data.username}`);
-      return;
-    }
+	socket.on("user_info", async (data) => {
+		User.findOne({ serverUserID: socket.id }).then((user) => {
+			if (!user) {
+				const user = new User({
+					serverUserID: `${socket.id}`,
+					username: `${data.username}`,
+					messages: "",
+				});
+				user.save();
+			}
+		});
+	});
+	socket.on("join_room", (data) => {
+		socket.join(data.room);
 
-    const room = new Rooms({
-      room_number: data.room,
-      sent_by_user: user._id,
-      username: data.username,
-      message: data.message
-    });
+		User.findOne({ username: data.username }).then((user) => {
+			if (!user) {
+				console.log(`No user found with username: ${data.username}`);
+				return;
+			}
 
-    room
-      .save()
-      .then(() => {
-        console.log(`Room created successfully @ ${room.room_number}`);
-      })
-      .catch((err) => console.log("Error creating room", err));
-  });
-  });
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
+			const room = new Rooms({
+				room_number: data.room,
+				sent_by_user: user._id,
+				username: data.username,
+				message: data.message,
+			});
 
-    console.log({
-      USER_MESSAGE: `User ${socket.id} sent ${data.message} to room ${data.room} @${data.timestamp}`,
-    });
-    User.findOneAndUpdate(
-      { serverUserID: socket.id },
-      { $set: { messages: data.message } },
-      { new: true, useFindAndModify: false }
-    )
-      .then((updatedPerson) => {
-        console.log("Updated Person", updatedPerson);
-      })
-      .catch((err) => {
-        console.error("Error updating Person", err);
-      });
-  });
-  socket.on("leave", async (data) => {
-    socket.disconnect();
-    User.collection.drop();
-    Rooms.collection.drop();
-    numUsers--;
-    // Fetch the user details before deleting
-    const searchPerson = await User.findOne({
-      serverUserID: `${socket.id}`,
-    });
-    if (searchPerson) {
-      console.log({
-        USER_DISCONNECTED: `USERNAME ${searchPerson.username} (${socket.id}) Disconnected from Room: ${data}`,
-        USERS_REMAINING: numUsers,
-      });
-      // Delete the user's data from the collection
-      await User.deleteOne({ serverUserID: `${socket.id}` });
-      console.log({
-        USER_DELETED: `User ${socket.id} data has been deleted from the database`,
-      });
-    } else {
-      console.log({ ERROR: `No user found with ID ${socket.id}` });
-    }
-  });
+			room
+				.save()
+				.then(() => {
+					console.log(`Room created successfully @ ${room.room_number}`);
+				})
+				.catch((err) => console.log("Error creating room", err));
+		});
+	});
+	socket.on("send_message", (data) => {
+		socket.to(data.room).emit("receive_message", data);
+
+		console.log({
+			USER_MESSAGE: `User ${socket.id} sent ${data.message} to room ${data.room} @${data.timestamp}`,
+		});
+		User.findOneAndUpdate(
+			{ serverUserID: socket.id },
+			{ $set: { messages: data.message } },
+			{ new: true, useFindAndModify: false }
+		)
+			.then((updatedPerson) => {
+				console.log("Updated Person", updatedPerson);
+			})
+			.catch((err) => {
+				console.error("Error updating Person", err);
+			});
+	});
+	socket.on("leave", async (data) => {
+		socket.disconnect();
+		User.collection.drop();
+		Rooms.collection.drop();
+		numUsers--;
+		// Fetch the user details before deleting
+		const searchPerson = await User.findOne({
+			serverUserID: `${socket.id}`,
+		});
+		if (searchPerson) {
+			console.log({
+				USER_DISCONNECTED: `USERNAME ${searchPerson.username} (${socket.id}) Disconnected from Room: ${data}`,
+				USERS_REMAINING: numUsers,
+			});
+			// Delete the user's data from the collection
+			await User.deleteOne({ serverUserID: `${socket.id}` });
+			console.log({
+				USER_DELETED: `User ${socket.id} data has been deleted from the database`,
+			});
+		} else {
+			console.log({ ERROR: `No user found with ID ${socket.id}` });
+		}
+	});
 });
 
 server.listen(3001, () => {
-  console.log("Server is running");
+	console.log("Server is running");
 });
