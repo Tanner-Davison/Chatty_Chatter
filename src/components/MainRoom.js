ebb105272d67f3f16221e0ed85b539242acdadde
Tanner Davison <Tboydavison@gmail.com>
2023-10-07 00:32:48 -0600

    fixed css

--- src/components/MainRoom.js
+++ src/components/MainRoom.js
@@ -1,13 +1,12 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import { LoginContext } from "./contexts/LoginContext";
 import "./MainRoom.css";
 import "../App.css";
 import { loadRoomHistory } from "./Utility-mainRoom/loadRoomHistory";
 import getCurrentTime from "./Utility-mainRoom/getTime";
-import io from 'socket.io-client';
+import io from "socket.io-client";
 import { getCurrentTimeJSX } from "./Utility-mainRoom/getTime";
 import { useNavigate } from "react-router-dom";
-
 
 const MainRoom = () => {
   const {
@@ -16,17 +15,18 @@ const MainRoom = () => {
     mainAccess,
     setMainAccess,
     socket,
-    setSocket
-    
+    setSocket,
   } = useContext(LoginContext);
-  
+
   const [message, setMessage] = useState("");
   const [messageRecieved, setMessageRecieved] = useState([]);
   const [room, setRoom] = useState(1);
-  const useTempName = JSON.parse(localStorage.getItem('username'))
- 
-  const [currentTime, setCurrentTime] = useState(getCurrentTimeJSX())
-  const [isSocketConnected, setSocketConnected] = useState(false)
+  const useTempName = JSON.parse(localStorage.getItem("username"));
+  const messagesStartRef = useRef(null);
+
+  const currentTime = getCurrentTimeJSX();
+  const [isSocketConnected, setSocketConnected] = useState('Disconnected');
+
   const navigate = useNavigate();
 
   const joinRoom = async () => {
@@ -37,25 +37,26 @@ const MainRoom = () => {
     });
     const messages = await loadRoomHistory(room);
     setMessageRecieved(messages);
+
   };
   const roomChanger = (event) => {
-  // setMessageRecieved("")
-	setRoom(event.target.value);
-};
+    // setMessageRecieved("")
+    setRoom(event.target.value);
+  };
   const sendUserInfo = (userLoginInfo) => {
     socket.emit("user_info", userLoginInfo);
   };
   const deleteRoom = () => {
     setMainAccess(true);
-    socket.emit("deleteRoom",{room: room, username:userLoginInfo.username});
+    socket.emit("deleteRoom", { room: room, username: userLoginInfo.username });
     socket.off("join_room", room);
-   navigate('/currentServers')
+    navigate("/currentServers");
   };
   const leaveRoom = () => {
-    socket.emit("leaveroom", room)
+    socket.emit("leaveroom", room);
     socket.disconnect();
-    navigate('/currentservers')
-  }
+    navigate("/currentservers");
+  };
   const sendMessageFunc = () => {
     // Emit the message
     const newMessage = {
@@ -65,24 +66,22 @@ const MainRoom = () => {
       username: userLoginInfo.username, // add this line
       sentBy: userLoginInfo.username,
     };
-    
+
     socket.emit("send_message", newMessage);
-   
+
     setMessageRecieved((prev) => [...prev, newMessage]);
     loadRoomHistory(room);
-    
   };
- 
- 
+
   useEffect(() => {
-    if (!socket){
-      console.log('there is no active socket')
+    if (!socket) {
+      console.log("there is no active socket");
       setSocket(io.connect("http://localhost:3001"));
       setUserLoginInfo({
-       username: JSON.parse(localStorage.getItem('username')),
-       password: JSON.parse(localStorage.getItem('password')),
-     });
-      return 
+        username: JSON.parse(localStorage.getItem("username")),
+        password: JSON.parse(localStorage.getItem("password")),
+      });
+      return;
     } // Prevent code from running if socket is null or undefined
 
     if (mainAccess === true) {
@@ -90,48 +89,55 @@ const MainRoom = () => {
       joinRoom();
       setMainAccess(false);
     }
-    socket.on("connection", () => {
-      setSocketConnected(socket.connected)
-    })
-    socket.on("disconnect", () => setSocketConnected(socket.connected));
-    
+  socket.on("connect", () => {
+    setSocketConnected("Connected");
+  });
+
+   socket.on("disconnect", (reason) => {
+     setSocketConnected("Disconnected");
+   });
+
     socket.on("error", (error) => {
       console.error("Socket Error:", error);
     });
-    const handleReceiveMessage= (data)=>{
-      if(data.room !== room){
-        
-        return;
-  }
-setMessageRecieved((prev) => [
-  ...prev,
-  {
-    message: data.message,
-    username: data.username,
-    timestamp: getCurrentTime(),
-  },
-]);
-    }
-    socket.on("receive_message",handleReceiveMessage);
-      
-      // const messages = await loadRoomHistory(room);
-      // setMessageRecieved(messages);
    
-  
+    const handleReceiveMessage = (data) => {
+      if (data.room !== room) {
+        return;
+      }
+      setMessageRecieved((prev) => [
+        ...prev,
+        {
+          message: data.message,
+          username: data.username,
+          timestamp: getCurrentTime(),
+        },
+      ]);
+    };
+    socket.on("receive_message", handleReceiveMessage);
+
+    // const messages = await loadRoomHistory(room);
+    // setMessageRecieved(messages);
 
     return () => {
       socket.off("error");
       socket.off("join_room", joinRoom);
-      socket.off("receive_message",handleReceiveMessage);
+      socket.off("receive_message", handleReceiveMessage);
     };
     // eslint-disable-next-line
   }, [socket, messageRecieved, isSocketConnected]);
   useEffect(() => {
     if (!userLoginInfo) {
-      loadRoomHistory(room)
+      loadRoomHistory(room);
     }
   });
- 
+  useEffect(() => {
+    if (messagesStartRef.current) {
+      messagesStartRef.current.scrollTop =
+        messagesStartRef.current.scrollHeight;
+    }
+  }, [messageRecieved]);
+
   return (
     <div className="App">
       <div className="header">
@@ -139,12 +145,8 @@ setMessageRecieved((prev) => [
           <h2 style={{ color: "white" }}>{userLoginInfo.username} </h2>
 
           <div className={"room-num-input"}>
-            <h2 style={{color:'white'}}>create Your own server!</h2>
-            <input
-              placeholder="Room #"
-              value={room}
-              onChange={roomChanger}
-            />
+            <h2 style={{ color: "white" }}>create Your own server!</h2>
+            <input placeholder="Room #" value={room} onChange={roomChanger} />
             <button type="submit" onClick={joinRoom}>
               Join Room
             </button>
@@ -154,59 +156,65 @@ setMessageRecieved((prev) => [
         <div className={"room-num-input"}>
           <input
             placeholder={message !== "" ? message : "Message..."}
-            onChange={event => setMessage(event.target.value)}
+            onChange={(event) => setMessage(event.target.value)}
           />
           <button onClick={sendMessageFunc}>Send Message</button>
         </div>
-        <div className={"all-messages"}>
-
+        <div className={"all-messages"} ref={messagesStartRef}>
           {messageRecieved.map((msg, index) => {
             if (msg.sentBy === userLoginInfo.username) {
-              
               // Message sent by current user
               return (
-                
-                  <div key={index} className={"messagesContainer"}>
-                    <div className={"container blue"}>
-                      <div className={"message-blue"}>
-                        <p className={"message-content"}>{msg.message}</p>
-                      </div>
-                      <p className={"user"}>{userLoginInfo.username}</p>
-                      <div className={"message-timestamp-left"}>
-                        <p>Sent:{currentTime}</p>
-                      </div>
+                <div key={index} className={"messagesContainer"}>
+                  <div className={"container blue"}>
+                    <div className={"message-blue"}>
+                      <p className={"message-content"}>{msg.message}</p>
+                    </div>
+                    <p className={"user"}>{userLoginInfo.username}</p>
+                    <div className={"message-timestamp-left"}>
+                      <p>Sent:{currentTime}</p>
                     </div>
                   </div>
-               
+                </div>
               );
             } else {
               // Message received from another user
               return (
-                
-                  <div key={index} className={"messagesContainer"}>
-                    <div className={"container green"}>
-                      <div className={"message-green"}>
-                        <p className={"message-content"}>{msg.message}</p>
-                      </div>
-                      <p className={"user"}>
-                        {msg.sentBy ? msg.sentBy : useTempName}
-                      </p>
-                      <div className={"message-timestamp-right"}>
-                        <p>Delivered: {msg.timestamp}</p>
-                      </div>
+                <div key={index} className={"messagesContainer"}>
+                  <div className={"container green"}>
+                    <div className={"message-green"}>
+                      <p className={"message-content"}>{msg.message}</p>
+                    </div>
+                    <p className={"user"}>
+                      {msg.sentBy ? msg.sentBy : useTempName}
+                    </p>
+                    <div className={"message-timestamp-right"}>
+                      <p>Delivered: {msg.timestamp}</p>
                     </div>
                   </div>
+                </div>
               );
             }
           })}
         </div>
       </div>
-      <button onClick={leaveRoom} >
-        Leave Room
-      </button>
-      <button
-      onClick={deleteRoom}> Delete Room </button>
-    <div>{isSocketConnected ? (<h1>Connected</h1>): (<h1>No Connection</h1>)}</div>
+      <button onClick={leaveRoom}>Leave Room</button>
+      <button onClick={deleteRoom}> Delete Room </button>
+      <div style={{display:'flex',alignItems:'center',justifyContent:'center',gap:'8px'}}>
+        {
+          <>
+            <h3>Status:</h3>
+            <h2
+              style={
+                isSocketConnected === "Connected"
+                  ? { color: "green" }
+                  : { color: "red" }
+              }>
+              {isSocketConnected}
+            </h2>
+          </>
+        }
+      </div>
     </div>
   );
 };
