ccc3d68676ce232223a20a04afe8361652953e84
Tanner Davison <Tboydavison@gmail.com>
2024-10-17 23:21:38 -0600

    updated

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build'));
 });
 // Create Server
 const server = http.createServer(app);
