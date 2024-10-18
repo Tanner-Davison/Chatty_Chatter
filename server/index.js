9a9357f6852ad16f5aa60da10b075b7043a02933
Tanner Davison <Tboydavison@gmail.com>
2024-10-18 00:07:26 -0600

    finally fixed it

--- server/index.js
+++ server/index.js
@@ -230,7 +230,7 @@ io.on("connection", (socket) => {
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
